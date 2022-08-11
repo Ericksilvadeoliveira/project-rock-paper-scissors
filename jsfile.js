905d3e9bb898a6926a977037e4442ee08331f0b6
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-08-11 18:29:00 -0300

    finished

--- jsfile.js
+++ jsfile.js
@@ -1,25 +1,62 @@
 let playerScore = 0;
 let computerScore = 0;
+let button = document.querySelectorAll('input');
 
 function getComputerChoice () {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound(playerSelection) {
-    let computerSelection = getComputerChoice();
+function disable() {
+    button.forEach(elem => {
+        elem.disabled = true;
+    })
+}
+function playRound(playerChoice) {
+    let computerChoice = getComputerChoice();
     let result = '';
 
-    if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) {
+    if ((playerChoice == 'rock' && computerChoice == 'scissors') || (playerChoice == 'paper' && computerChoice == 'rock') || (playerChoice == 'scissors' && computerChoice == 'paper')) {
 
         playerScore += 1;
 
-        result = 'You win!!!';
+        result = 'You win!!!<br></br>Player Score: ' + playerScore + '<br>Computer Score: ' + computerScore;
 
         if (playerScore == 5) {
-            result += 'You won the game!';
+            result = 'Player Score: ' + playerScore + '<br>Computer Score: ' + computerScore + '<br><br>You won the game!<br><br><br><br>The page will be reloaded in 5 seconds.';
+            disable();
+            
+            setTimeout(function() {
+                location.reload(true);
+            }, 5000);
+            
+            
         }
-    } else if (playerSelection == computerSelection) {
-        'it\'s a tie. You both chose ' + playerSelection;
+
+    } else if (playerChoice == computerChoice) {
+        result = 'it\'s a tie. You both chose ' + playerChoice + '<br></br>Player Score: ' + playerScore + '<br>Computer Score: ' + computerScore;
+
+    } else {
+        computerScore +=1;
+        result = 'You lose!<br></br>Player Score: ' + playerScore + '<br>Computer Score: ' + computerScore;
+
+        if (computerScore == 5) {
+            result = 'Player Score: ' + playerScore + '<br>Computer Score: ' + computerScore + '<br><br>I won the game!<br><br>The page will be reloaded in 5 seconds.';
+            disable();
+
+            setTimeout(function() {
+                location.reload(true);
+            }, 5000);
+
+        }
     }
+
+    document.getElementById('result').innerHTML = result;
+    return;
 }
+
+button.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
